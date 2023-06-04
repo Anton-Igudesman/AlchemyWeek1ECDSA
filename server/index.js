9c795b4a1059e2ac9d269a47c7cba4d57beecf88
Anton-Igudesman <antonguy@gmail.com>
2023-06-04 12:20:38 -0700

    fixing private key issue

--- server/index.js
+++ server/index.js
@@ -30,10 +30,10 @@ balances[walletAddress3] = 75;
 let transactionAlert = false;
 // console.log('wallet addresses', balances);
 
-// const privateKeys = {};
-// privateKeys[walletAddress1] = '906c363d3fd5618ed68cec14bf9eb15994a81c4ad1a99de88c7a400aec17f099';
-// privateKeys[walletAddress2] = 'b4bb05d80937308b6767eb9c75e22e624fec222b48843163d87a0d74714057bb';
-// privateKeys[walletAddress3] = 'a63a0fdbe0136806fc6458ed9bf861947960100cf50951cdf4a3225022ea6910';
+const privateKeys = {};
+privateKeys[walletAddress1] = '906c363d3fd5618ed68cec14bf9eb15994a81c4ad1a99de88c7a400aec17f099';
+privateKeys[walletAddress2] = 'b4bb05d80937308b6767eb9c75e22e624fec222b48843163d87a0d74714057bb';
+privateKeys[walletAddress3] = 'a63a0fdbe0136806fc6458ed9bf861947960100cf50951cdf4a3225022ea6910';
 
 // console.log('private keys: ', privateKeys);
 
@@ -42,6 +42,9 @@ let transactionAlert = false;
 //   const hash = keccak256(bytes);
 //   return toHex(hash);
 // }
+
+console.log(walletAddress1)
+console.log(walletAddress2)
 function verifyAddress (keyArray, walletAddress) {
   const derivedAddress = `0x${toHex(keccak256(keyArray.slice(1))).slice(-20)}`;
   return derivedAddress === walletAddress ? true : false;
